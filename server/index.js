e3f5aee56ced7c87b085686fa2de4d34d82b5546
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2024-03-16 23:28:04 +0600

    error fixed

--- server/index.js
+++ server/index.js
@@ -27,17 +27,12 @@ import orderRouter from './routes/orderRoutes.js'
 // import uploadRouter from './routes/uploadRoutes.js'
 
 dbConnect()
-app.use(morgan('dev'))
 app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(
-  bodyParser.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf
-    },
-  })
-)
 app.use(cookieParser())
+
 app.use('/api/user', authRouter)
 app.use('/api/product', productRouter)
 app.use('/api/blog', blogRouter)
